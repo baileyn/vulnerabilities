8b35fcc495229fdb7dafabcec238680788cd82b3
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-23 22:24:26 -0400

    feat(vuln): add an incredible simple vulnerable php login page

--- basic/html/index.php
+++ basic/html/index.php
@@ -1,11 +1,42 @@
+<?php
+$servername = "db";
+$username = "root";
+$password = "password";
+$db = "vulns";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+?>
+
 <html>
 <head>
 </head>
 <body>
     <?if (isset($_POST['submit'])) {?>
-        <p>Hello, <?=$_POST['username']?></p>
-    <?} else {?>
-        <?include('login-form.php')?>
+        <?php
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $q = "SELECT id FROM users WHERE username = '$username' AND password = '$password'";
+        echo $q;
+        
+        $results = $conn->query($q);
+        if ($results->num_rows == 1) {
+            echo "<h1>Welcome, $username</h1>";
+        } else {
+            echo "<p>Incorrect username/password!</p>";
+        }
+        ?>
     <?}?>
+    <form action="<?=$_SELF?>" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username">
+        <label for="password">Password:</label>
+        <input type="text" name="password">
+        <input type="submit" name="submit">
+    </form>
 </body>
 </html>
